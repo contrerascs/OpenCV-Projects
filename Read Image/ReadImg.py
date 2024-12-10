95c1ae953a051a31cbc185395e7d15571f0a4bc5
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-12-10 00:07:33 -0600

    Read image

--- Read Image/ReadImg.py
+++ Read Image/ReadImg.py
@@ -1,6 +1,7 @@
 import cv2
 
-imagen = cv2.imread('Gojo.jpg')
+imagen = cv2.imread(r'Read Image/Gojo.jpg',0)
 cv2.imshow('Prueba de imagen', imagen)
+cv2.imwrite(r'Read Image/Grises.jpg',imagen)
 cv2.waitKey(5000)
 cv2.destroyAllWindows()
