5a429761a540aa9821d1ecc3fbed2605d686c87a
contrerascs <60404702+contrerascs@users.noreply.github.com>
2024-12-17 00:44:49 -0600

    Detect multiple colors and tracking

--- Detect Color and Tracking/detectColorAndTracking.py
+++ Detect Color and Tracking/detectColorAndTracking.py
@@ -8,12 +8,26 @@ azulAlto = np.array([125,255,255], np.uint8)
 
 while True:
     ret, frame = cap.read()
-    
+
     if ret == True:
         frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        mask = cv2.add(frameHSV,azulBajo, azulAlto)
+        mask = cv2.inRange(frameHSV,azulBajo, azulAlto)
+        contornos,hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL,
+                                         cv2.CHAIN_APPROX_SIMPLE)
 
-        cv2.imshow('maskAzul',mask)
+        for c in contornos:
+            area = cv2.contourArea(c)
+            if area > 300:
+                M = cv2.moments(c)
+                if (M['m00']==0): M['m00']=1
+                x = int(M['m10']/M['m00'])
+                y = int(M['m01']/M['m00'])
+                cv2.circle(frame,(x,y),7,(0,255,0),-1)
+                fort = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(frame,'{},{}'.format(x,y),(x+10,y),fort,0.75,(0,255,0),1,cv2.LINE_AA)
+                nuevoContorno = cv2.convexHull(c)
+                cv2.drawContours(frame,[nuevoContorno],0,(255,0,0),3)
+        #cv2.imshow('maskAzul',mask)
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('s'):
             break
